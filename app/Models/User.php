225c5676cb75cd861920901cca7897e2a58cbdcb
Seyit Mustafa Adem KANDEMİR <seyitmustafaademk@gmail.com>
2024-04-27 22:44:21 +0300

    added note relation in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function notes(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Note::class);
+    }
 }
