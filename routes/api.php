156acadf423adc94f7d846d85b73f5e5b3102711
Seyit Mustafa Adem KANDEMİR <seyitmustafaademk@gmail.com>
2024-04-28 21:08:07 +0300

    created resource and resource collection for note apis

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\NoteController;
 
-Route::prefix('/auth')->name('auth.')->group(function () {
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+Route::prefix('/')->middleware('auth:sanctum')->group(function () {
+    Route::prefix('/auth')->name('auth.')->group(function () {
+        Route::post('/register', [AuthController::class, 'register'])->name('register')->withoutMiddleware('auth:sanctum');
+        Route::post('/login', [AuthController::class, 'login'])->name('login')->withoutMiddleware('auth:sanctum');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
+
+    Route::prefix('/note')->name('notes.')->group(function () {
+        Route::apiResource('/', NoteController::class);
+    });
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
